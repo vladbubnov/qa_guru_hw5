df5facc79239850a03fe616b27f0ea94d19ca9c7
vladislav.bubnov <v.bubnov@yclients.tech>
2024-11-26 08:51:30 +0200

    add window size

--- confest.py
+++ confest.py
@@ -4,6 +4,8 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
 
     yield
