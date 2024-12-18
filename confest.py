217b47329c7b956c2fc5fe928a104db6e75e6d98
vladislav.bubnov <v.bubnov@yclients.tech>
2024-12-18 17:11:14 +0200

    add attach

--- confest.py
+++ confest.py
@@ -6,8 +6,36 @@ from selenium.webdriver.chrome.options import Options
 from utils import attach
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
+# @pytest.fixture(scope='function')
+# def setup_browser(request):
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "100.0",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+#     browser.config.driver = driver
+#
+#     yield browser
+#
+#     attach.add_screenshot(browser)
+#     attach.add_logs(browser)
+#     attach.add_html(browser)
+#     attach.add_video(browser)
+#
+#     browser.quit()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -24,19 +52,15 @@ def setup_browser(request):
     )
     browser.config.driver = driver
 
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    browser.config.base_url = 'https://demoqa.com'
+
     yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
-    browser.config.base_url = 'https://demoqa.com'
-
-    yield
+    attach.add_video(browser)
 
     browser.quit()
