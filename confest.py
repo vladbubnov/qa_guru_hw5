32291945f153f14c65b980db8caa656baa702ad9
vladislav.bubnov <v.bubnov@yclients.tech>
2024-12-18 17:13:09 +0200

    optimize fixture

--- confest.py
+++ confest.py
@@ -6,34 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from utils import attach
 
 
-# @pytest.fixture(scope='function')
-# def setup_browser(request):
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "100.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#     browser.config.driver = driver
-#
-#     yield browser
-#
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     attach.add_video(browser)
-#
-#     browser.quit()
-
-
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     options = Options()
